ff6373d1ec7d10ba0f4a56742997307f491f9669
schou <pschou@users.noreply.github.com>
2023-05-29 23:15:45 -0400

    add about

--- merge-scanner/merge.go
+++ merge-scanner/merge.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 github.com/pschou/go-bloom
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// Merge sorter for Text Scanners.  Given multiple lists of sorted entities,
+// this will maintain the sort and merge the lists together.
 
 package merge
 
